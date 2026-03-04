b0a42fe6b07867c8dcfc8ff010bb29642096b3bc
kohsweeleong <kohsweeleong@gmail.com>
2026-03-04 21:45:55 +0800

    Update q4.py

--- q4.py
+++ q4.py
@@ -6,7 +6,12 @@ def string_reverse(s):
     - Return the reversed string.
     """
     return
-
+# Validate the input type
+    if not isinstance(s, str):
+        return "Error: Input is not a string"
+        
+    # Reverse the string using Python's slicing feature [start:stop:step]
+    return s[::-1]
 
 # Task 2
 # Invoke the function "string_reverse" using the following scenarios:
